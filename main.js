6685e19f984c12ee03db4ae619ef610070bf86f9
David Heidari <davidheidari2@gmail.com>
2023-11-25 16:23:00 +0100

    change som styling

---
+++ main.js
@@ -0,0 +1,16 @@
+const titel = document.getElementById('titel');
+
+titel.addEventListener('click', function () {
+    
+    titel.style.width = '50%';
+    titel.style.display = 'flex'
+    titel.style.alignItems = 'center';
+    titel.style.justifyContent = 'center';
+    titel.style.margin = '2rem auto';
+    titel.style.padding = '10px 15px';
+    titel.style.borderRadius = '8px';
+    titel.style.color = 'white';
+    titel.style.letterSpacing = '8px';
+
+
+});
